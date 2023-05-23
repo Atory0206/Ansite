1e00ba16171eac26dbea0aaa7be4922410d772c6
DESKTOP-S793ICQ\89 <230233@naver.com>
2023-05-23 11:37:38 +0900

    https://eveningdev.tistory.com/6

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -106,3 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR, "static",
+]
